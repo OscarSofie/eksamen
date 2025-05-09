7ea50447511bb73931ec93953947f5ee7da58a71
Sofie <Soov0001@stud.kea.dk>
2025-05-09 13:28:18 +0200

    api page

--- src/app/api/page.js
+++ src/app/api/page.js
@@ -1,10 +1,19 @@
 export async function getEvents() {
-  const res = await fetch("https://api.smk.dk/api/v1/art")
+  const res = await fetch("https://eksamenso.onrender.com/events");
   const data = await res.json();
-  console.log(data)
-  return data;
+  return data.events;
 }
 
-getEvents()
+export async function getSearchResults(query) {
+  const res = await fetch(`https://api.smk.dk/api/v1/art/search?keys=${query}`);
+  const data = await res.json();
+  return data.items;
+}
 
-
+export async function getSingleArtwork(query) {
+  const res = await fetch(
+    `https://api.smk.dk/api/v1/art?object_number=${object_number}`
+  );
+  const data = await res.json();
+  return data.items;
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
-
+const res = await fetch("https://api.smk.dk/api/v1/art?object_number=KKS5261");
+const data = await res.json();
+console.log(data);
 
 export default function Home() {
-  return (
-    <></>
-  );
+  return <></>;
 }
