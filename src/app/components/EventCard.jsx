e3063a9ea9c0053a0d1a026820e493c6b4415601
Sofie <Soov0001@stud.kea.dk>
2025-05-14 10:26:24 +0200

    responsivt singleview

--- src/app/components/EventCard.jsx
+++ src/app/components/EventCard.jsx
@@ -1,20 +1,59 @@
 import Link from "next/link";
 import { getSingleArtwork } from "../api/page";
+import Image from "next/image";
+import Button from "./Button";
+import { SignedIn } from "@clerk/nextjs";
 
-const EventCard = async ({ event }) => {
+const EventCard = async ({
+  event,
+  title,
+  description,
+  date,
+  image,
+  isCurator,
+}) => {
   const artworks = await getSingleArtwork(event.artworkIds[0]);
 
   return (
-    <div>
+    <div className="bg-[#F5F3EC] p-4 w-xs flex flex-col ">
       <Link href={`/events/${event.id}`}>
         {artworks.map((art) => (
-          <img key={event.id} src={art.image_thumbnail} alt="" />
+          <div>
+            <Image
+              key={event.id}
+              src={art.image_thumbnail}
+              alt={event.title}
+              width={300}
+              height={400}
+              className="w-full h-auto"
+            ></Image>
+          </div>
         ))}
 
-        <h1>{event.title}</h1>
-        <h2>{event.date}</h2>
-        <p>{event.description}</p>
+        <h1 className="text-4xl font-semibold mt-3 text-[#000342]">
+          {event.title}
+        </h1>
+        <h2 className="text-xs text-[#000342] opacity-50 mt-3">{event.date}</h2>
+        <p className="text-sm text-[#000342] mt-5 font-light">
+          {event.description}
+        </p>
       </Link>
+      <div>
+        <Link href={`/events/${event.id}`}>
+          <Button variant="secondary">Læs mere om udstillingen</Button>
+        </Link>
+
+        <SignedIn>
+          <Link href="/secret">
+            <Button variant="primary">Rediger event</Button>
+          </Link>
+          {isCurator === true ? (
+            <Link href="/secret">
+              <Button variant="primary">Rediger event</Button>
+            </Link>
+          ) : null}
+        </SignedIn>
+      </div>
     </div>
   );
 };
