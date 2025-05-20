df64b2b0c11cc9ac0875687b6bbf347a6bd12fc8
Oscar <albertlund121@gmail.com>
2025-05-20 20:31:37 +0200

    nu må den da kunne deploy

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+    <ClerkProvider >
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
