10c9b30c73e9c46834303face2ace32ec1b6efe2
Oscar <albertlund121@gmail.com>
2025-05-20 19:30:58 +0200

    Adds event update functionality and improves API handling
    Introduces a function to update events, including redirection after updates. Enhances API request handling by encoding parameters for better reliability. Fixes incorrect parameter handling in `getSingleEvent` and optimizes artwork query construction.
    
    No related issue specified.

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -3,7 +3,7 @@
 import { redirect } from 'next/navigation';
 import { createEvent } from '@/api/page';
 import { deleteEvent } from '@/api/page';
-
+import { updateEvent } from '@/api/page';
 
 export async function opretEvent(formData) {
   const title = formData.get('title');
@@ -30,6 +30,28 @@ export async function opretEvent(formData) {
   redirect(`/events/${newEvent.id}`);
 }
 
+export async function redigerEvent(formData) {
+  const id = formData.get('id');
+  const title = formData.get('title');
+  const description = formData.get('description');
+  const curator = formData.get('curator');
+  const date = formData.get('date');
+  const locationId = formData.get('locationId');
+  const artworkIdsString = formData.get('artworkIds');
+  const artworkIds = JSON.parse(artworkIdsString || '[]');
+
+  const updatedData = {
+    title,
+    description,
+    curator,
+    date,
+    locationId,
+    artworkIds,
+  };
+
+  await updateEvent(id, updatedData);
+  redirect(`/events/${id}`);
+}
 
 export async function sletEvent (formData) {
   const id = formData.get("eventId");
