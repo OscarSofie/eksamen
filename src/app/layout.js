53ee15ac27870c8cdf27bb048f1b6b26bd25c816
Oscar <albertlund121@gmail.com>
2025-05-20 20:37:24 +0200

    nu?

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider >
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY} >
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
