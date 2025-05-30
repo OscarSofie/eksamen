845876ec3419e04998c513f0fa001064dfd5e45e
Oscar <albertlund121@gmail.com>
2025-05-30 13:10:39 +0200

    client component til bestilling

--- src/app/bestilling/page.jsx
+++ src/app/bestilling/page.jsx
@@ -1,19 +1,14 @@
-"use client";
 
-import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import Bestilling from "../components/Bestilling";
 
-export default function BekræftelsePage() {
-  const searchParams = useSearchParams();
-  const email = searchParams.get("email");
-  const orderId = searchParams.get("orderId");
 
+
+export default function Page() {
+ 
   return (
-    <div className="p-10 text-center">
-      <h1 className="text-4xl font-bold mb-4">Tak for din tilmelding</h1>
-      <p className="text-xl">Kære {email}, tak for din tilmelding.</p>
-      <p className="text-md mt-2">
-        Din ordrebekræftelse har ID: <strong>{orderId}</strong>
-      </p>
-    </div>
+    <Suspense fallback={<div>Indlæser bestilling...</div>}>
+      <Bestilling />
+    </Suspense>
   );
 }
